468935e10b17b5733d4d398e2e15c8592670c12d
thelastofus3 <alekseenkomaxim456@gmail.com>
2024-11-19 22:09:53 +0100

    update

--- js/room.js
+++ js/room.js
@@ -10,67 +10,63 @@ const chatButton = document.getElementById('chat__button');
 let activeMemberContainer = false;
 
 memberButton.addEventListener('click', () => {
-  if (activeMemberContainer) {
-    memberContainer.style.display = 'none';
-  } else {
-    memberContainer.style.display = 'block';
-  }
+    if (activeMemberContainer) {
+        memberContainer.style.display = 'none';
+    } else {
+        memberContainer.style.display = 'block';
+    }
 
-  activeMemberContainer = !activeMemberContainer;
+    activeMemberContainer = !activeMemberContainer;
 });
 
 let activeChatContainer = false;
 
 chatButton.addEventListener('click', () => {
-  if (activeChatContainer) {
-    chatContainer.style.display = 'none';
-  } else {
-    chatContainer.style.display = 'block';
-  }
+    if (activeChatContainer) {
+        chatContainer.style.display = 'none';
+    } else {
+        chatContainer.style.display = 'block';
+    }
 
-  activeChatContainer = !activeChatContainer;
+    activeChatContainer = !activeChatContainer;
 });
 
-let displayFrame = document.getElementById('stream__box')
-let videoFrames = document.getElementsByClassName('video__container')
-let userIdInDisplayFrame = null;
+let displayFrame = document.getElementById('stream__box');
+let videoFrames = document.getElementsByClassName('video__container');
+let userIdDisplayFrame = null;
 
 let expandVideoFrame = (e) => {
-
-  let child = displayFrame.children[0]
-  if(child){
-      document.getElementById('streams__container').appendChild(child)
-  }
-
-  displayFrame.style.display = 'block'
-  displayFrame.appendChild(e.currentTarget)
-  userIdInDisplayFrame = e.currentTarget.id
-
-  for(let i = 0; videoFrames.length > i; i++){
-    if(videoFrames[i].id != userIdInDisplayFrame){
-      videoFrames[i].style.height = '100px'
-      videoFrames[i].style.width = '100px'
+    let child = displayFrame.children[0];
+    if (child) {
+        document.getElementById('streams__container').appendChild(child);
     }
-  }
+    displayFrame.style.display = 'block';
+    displayFrame.appendChild(e.currentTarget);
+    userIdDisplayFrame = e.currentTarget.id;
 
+    for (let i = 0; videoFrames.length > i; i++) {
+        if (videoFrames.id !== userIdDisplayFrame) {
+            videoFrames[i].style.height = '100px';
+            videoFrames[i].style.width = '100px'
+        }
+    }
 }
 
-for(let i = 0; videoFrames.length > i; i++){
-  videoFrames[i].addEventListener('click', expandVideoFrame)
+for (let i = 0; videoFrames.length > i; i++) {
+    videoFrames[i].addEventListener('click', expandVideoFrame);
 }
-
 
 let hideDisplayFrame = () => {
-    userIdInDisplayFrame = null
-    displayFrame.style.display = null
+    userIdDisplayFrame = null;
+    displayFrame.style.display = null;
 
-    let child = displayFrame.children[0]
-    document.getElementById('streams__container').appendChild(child)
+    let child = displayFrame.children[0];
+    document.getElementById('streams__container').appendChild(child);
 
-    for(let i = 0; videoFrames.length > i; i++){
-      videoFrames[i].style.height = '300px'
-      videoFrames[i].style.width = '300px'
+  for (let i = 0; videoFrames.length > i; i++) {
+    videoFrames[i].style.height = '300px';
+    videoFrames[i].style.width= '300px';
   }
 }
 
-displayFrame.addEventListener('click', hideDisplayFrame)
+displayFrame.addEventListener('click', hideDisplayFrame);
